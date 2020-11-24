266a34999eb7a6ec50dc34dc03a0a05d8420b93d
nguyenphucluan <58660258+nguyenphucluan@users.noreply.github.com>
2020-11-23 23:50:41 -0800

    validformlogin.js

--- Final Project/phpcode/main.js
+++ Final Project/phpcode/main.js
@@ -1,1 +1,12 @@
-// Our JavaScript and jQuery here
+function validateForm() {
+	var user = document.myform.username.value;
+    var pass = document.myform.password.value;
+    if ( user == "" || pass == "") {
+        alert("Please Fill All Information");
+        return false;
+    }
+    if (pass.length < 6  ) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+}
