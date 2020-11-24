32bf0e7ec4db699c2815e3098aa4c87a67628116
levip01217 <73676403+levip01217@users.noreply.github.com>
2020-11-24 16:03:07 +0700

    Update main.js

--- Final Project/phpcode/main.js
+++ Final Project/phpcode/main.js
@@ -1,3 +1,4 @@
+/* validform login */
 function validateForm() {
 	var user = document.myform.username.value;
     var pass = document.myform.password.value;
@@ -7,6 +8,47 @@ function validateForm() {
     }
     if (pass.length < 6  ) {
         alert("Password must be at least 6 characters long.");
+        return false;
+    }
+}
+/* validform register */
+function validateForm() {
+	var user = document.myform.username.value;
+        var pass = document.myform.password.value;
+        var addre = document.myform.address.value;
+        var pass2 = document.myform.password2.value;
+        var fname = document.myform.fullname.value;
+        var phonee = document.myform.phone.value;
+        var images = document.myform.image.value;
+        var date = document.myform.dateofbirth.value;
+        var rol = document.myform.role.value;
+                
+		if ( phonee == "" || images == "" || date == "" || rol == "" || user == "" || fname =="" || addre == "") {
+                    alert("Please Fill All Information");
+                    return false;
+                }
+                if (pass.length < 6  ) {
+                    alert("Password must be at least 6 characters long.");
+                    return false;
+                }
+                if (pass2.length < 6 ) {
+                    alert("Password must be at least 6 characters long.");
+                    return false;
+                }
+                if (pass==pass2) {
+                    return true;
+                }
+                else if (pass!=pass2) {
+                    alert("password must be same!")
+                    return false;
+                }
+            }   
+/* validform lostpassword */
+function validateForm() {
+	var user = document.myform.username.value;
+	var phonee = document.myform.phone.value;
+	if ( phonee == ""|| user == ""  ) {
+        alert("Please Fill All Information");
         return false;
     }
 }
