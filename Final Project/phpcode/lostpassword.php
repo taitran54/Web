c99eb014d19e7a6fbe08152f5590abc1d1c1bc06
nguyenphucluan <58660258+nguyenphucluan@users.noreply.github.com>
2020-11-28 04:33:38 -0800

    Update lostpassword.php

--- Final Project/phpcode/lostpassword.php
+++ Final Project/phpcode/lostpassword.php
@@ -1,8 +1,99 @@
-<html>
+
+<html lang="en">
 <head>
     <title>Forgot Password</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="main.js"></script>
+    
+    <style type="text/css">
+        body{
+            margin: 0;
+            padding: 0;
+            background: url(image/theme.jpg);
+            background-size: cover;
+            background-position: center;
+            font-family: sans-serif;
+        }
+        .forgotpassword{
+            width: 320px;
+            height: 420px;
+            background: #000;
+            color: #fff;
+            top: 50%;
+            left: 50%;
+            position: absolute;
+            transform: translate(-50%,-50%);
+            box-sizing: border-box;
+            padding: 70px 30px;
+        }
+        .avatar{
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            position: absolute;
+            top: -50px;
+            left: calc(50% - 50px);
+        }
+        h1{
+            margin: 0;
+            padding: 0 0 20px;
+            text-align: center;
+            font-size: 22px;
+        }
+        .forgotpassword p{
+            margin: 0;
+            padding: 0;
+            font-weight: bold;
+        }
+        .forgotpassword input{
+            width: 100%;
+            margin-bottom: 20px;
+        }
+        .forgotpassword input[type="text"], input[type="password"]
+        {
+            border: none;
+            border-bottom: 1px solid #fff;
+            background: transparent;
+            outline: none;
+            height: 40px;
+            color: #fff;
+            font-size: 16px;
+        }
+        .forgotpassword input[type="submit"]
+        {
+            border: none;
+            outline: none;
+            height: 40px;
+            background: #fb2525;
+            color: #fff;
+            font-size: 18px;
+            border-radius: 20px;
+        }
+        .forgotpassword input[type="submit"]:hover
+        {
+            cursor: pointer;
+            background: #ffc107;
+            color: #000;
+        }
+        .forgotpassword a{
+            text-decoration: none;
+            font-size: 12px;
+            line-height: 20px;
+            color: darkgrey;
+        }
+       .forgotpassword a:hover
+        {
+            color: #ffc107;
+        }
+    </style>
+    <script type="text/javascript">
+        function validateLostPassForm() {
+            var user = document.myLostPassForm.username.value;
+            var phonee = document.myLostPassForm.phone.value;
+            if ( phonee == ""|| user == ""  ) {
+                alert("Please Fill All Information");
+                return false;
+            }
+        }
+    </script>
 </head>
 <body>
     <main>
