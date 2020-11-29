b06ab86bbfa29f079cab9b8ca36d68766d8241a9
Tran Nhan Tai <69810464+amyezio@users.noreply.github.com>
2020-11-29 13:17:52 +0700

    modify

---
+++ Final Project/phpcode/processjoin.php
@@ -0,0 +1,23 @@
+<?php
+$code = $_POST['code'];
+require "function.php";
+session_start();
+$id_account = getId($_SESSION["username"]);
+
+require "connection.php";
+$sql = "SELECT C.id FROM Class C WHERE C.code = ?";
+$stm = $conn ->prepare ($sql);
+$stm -> bind_param ('s', $code);
+$stm ->execute();
+$result = $stm -> get_result();
+if ($row = $result->fetch_assoc()){
+    $id_class=$row[id];
+    $sql = "INSERT INTO Joining (id_account, id_class, approval)
+            VALUES ($id_account, $id_class, 0)";
+    if ($conn -> execute($sql)){
+        header("Location: Homepage.php");
+        exit;
+    }
+}
+header("Location: joinclass.php?error=code");
+?>
