13dcf90d4bffb742c05d10bb5fc13d950967fb61
TT2504 <thong85007@gmail.com>
2020-11-30 18:02:05 +0700

    .

--- Final Project/phpcode/register.php
+++ Final Project/phpcode/register.php
@@ -12,10 +12,8 @@
         </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
 	<link rel="stylesheet" href="style.css">
-</style>
-<script type="text/javascript" src="main.js"></script>
+	<script type="text/javascript" src="main.js"></script>
     </head>
     <body>
 
