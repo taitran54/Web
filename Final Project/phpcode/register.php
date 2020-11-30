1ab6aba023f7b52c1a29600216600b29f2154f44
levip01217 <73676403+levip01217@users.noreply.github.com>
2020-11-30 16:25:18 +0700

    Update register.php

--- Final Project/phpcode/register.php
+++ Final Project/phpcode/register.php
@@ -15,42 +15,7 @@
 <style>
 	<link rel="stylesheet" href="style.css">
 </style>
-        <script type="text/javascript">
-            
-            function validateRegisterForm() {
-                var user = document.myRegisterForm.username.value;
-                var pass = document.myRegisterForm.password.value;
-                var addre = document.myRegisterForm.address.value;
-                var pass2 = document.myRegisterForm.password2.value;
-                var fname = document.myRegisterForm.fullname.value;
-                var phonee = document.myRegisterForm.phone.value;
-                var images = document.myRegisterForm.image.value;
-                var date = document.myRegisterForm.dateofbirth.value;
-                var rol = document.myRegisterForm.role.value;
-                
-                
-                if ( phonee == "" || images == "" || date == "" || rol == "" || user == "" || fname =="" || addre == "") {
-                    alert("Please Fill All Information");
-                    return false;
-                }
-                if (pass.length < 6  ) {
-                    alert("Password must be at least 6 characters long.");
-                    return false;
-                }
-                if (pass2.length < 6 ) {
-                    alert("Password must be at least 6 characters long.");
-                    return false;
-                }
-                if (pass==pass2) {
-                    return true;
-                }
-                else if (pass!=pass2) {
-                    alert("password must be same!")
-                    return false;
-                }
-            }   
-
-        </script>
+<script type="text/javascript" src="main.js"></script>
     </head>
     <body>
 
