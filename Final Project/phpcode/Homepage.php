c61906a3b5542ab10791ef92d46e1ca841aed58e
Tran Nhan Tai <69810464+amyezio@users.noreply.github.com>
2020-12-02 21:09:18 +0700

    modify

--- Final Project/phpcode/Homepage.php
+++ Final Project/phpcode/Homepage.php
@@ -56,6 +56,7 @@ require 'function.php';
         <a class="nav-link" href="logout.php">Logout</a>
       </li>
     </ul>
+	<br></br>
     <form class="form-inline my-2 my-lg-0" action="#" method="get">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" name="searchkey" aria-label="Search" value="<?php echo isset($_GET['searchkey'])? $_GET['searchkey'] : '' ?>">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
